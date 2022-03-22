702a173e2aff5fb557bb7b64d441b1140f6c7fb3
samanewman <36173258+samanewman@users.noreply.github.com>
2022-03-22 10:42:35 -0300

    Update ItemDetail.js

--- src/components/ItemDetail/ItemDetail.js
+++ src/components/ItemDetail/ItemDetail.js
@@ -1,6 +1,6 @@
 //Detalles del item, acá el llamado a la función para añadir un producto al carrito.
 
-import Item from '../item/item';
+import Item from '../item/Item';
 import './ItemDetail.css';
 import ItemCount from '../ItemCount/ItemCount';
 import {useContext, useState} from 'react';
